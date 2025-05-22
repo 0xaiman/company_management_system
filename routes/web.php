36366b88015a36485f3684310e91a6f29f747815
0xaiman <aimandani9797@gmail.com>
2025-05-23 00:04:35 +0800

    feat (routing): make root page redirects  to login page

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,10 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/login');
+});
+Route::get('/logout', function () {
+    return redirect('/login');
 });
 
 Route::get('/dashboard', function () {

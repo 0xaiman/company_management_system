f13505338090b761043dfbb01660fce8eb4d87d9
0xaiman <aimandani9797@gmail.com>
2025-05-22 13:44:13 +0800

    fix( migration: companies) : make column email unqiue instead of nullable

--- database/migrations/2025_05_22_053533_create_companies_table.php
+++ database/migrations/2025_05_22_053533_create_companies_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('companies', function (Blueprint $table) {
             $table->id();
             $table->string('name')->unique();
-            $table->string('email')->nullable();
+            $table->string('email')->unique();
             $table->string('logo')->nullable();
             $table->string('website_url')->nullable();
             $table->timestamps();

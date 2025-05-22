17799954fb9b545ee0a8af53963d20a8e896860b
0xaiman <aimandani9797@gmail.com>
2025-05-23 00:39:23 +0800

    fix(update seeder) : remove image url in seeder

--- database/seeders/CompanySeeder.php
+++ database/seeders/CompanySeeder.php
@@ -16,14 +16,12 @@ class CompanySeeder extends Seeder
         Company::create([
             'name' => 'Company 1',
             'email' => 'company1@mail.com',
-            'logo' => 'https://cdn.pixabay.com/photo/2023/02/01/00/54/company-7759278_1280.png',
             'website_url' => 'https://dummyurl1.com',
         ]);
 
         Company::create([
             'name' => 'Company 2',
             'email' => 'company2@mail.com',
-            'logo' => 'https://cdn.pixabay.com/photo/2018/04/01/09/28/image-3280358_1280.jpg',
             'website_url' => 'https://dummyurl2.com',
         ]);
     }

c7b382dc0ae516ab45776fa96c188c6134d68b45
0xaiman <aimandani9797@gmail.com>
2025-05-22 14:06:45 +0800

    feat (CompanyControler and Company Model) : Setup controler and eloquent Model

---
+++ app/Http/Controllers/CompanyController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Company;
+use Illuminate\Http\Request;
+
+class CompanyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $companies = Company::all();
+
+        return view('companies.index', compact('companies'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('companies.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:companies',
+            'website_url' => 'required|url',
+            'logo' => 'nullable|image|dimensions:min_width=100,min_height=100',
+        ]);
+
+        if($request->hasFile('logo')){
+            $validated['logo'] = $request->file('logo')->store('logos', 'public');
+        }
+
+        Company::create($validated);
+
+        return redirect()->route('companies.index')->with('success', 'Company created successfully');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Company $company)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Company $company)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Company $company)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Company $company)
+    {
+        //
+    }
+}
